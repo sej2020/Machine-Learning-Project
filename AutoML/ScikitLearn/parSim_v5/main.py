3c1be6eaa7752456d6bc66f3be9a35c7927223e1
sj110 <sj110@iu.edu>
2023-02-14 10:35:02 -0500

    file organization

--- AutoML/ScikitLearn/parSim_v5/main.py
+++ AutoML/ScikitLearn/parSim_v5/main.py
@@ -48,19 +48,3 @@ if __name__=="__main__":
     stop = perf_counter()
 
     print(f"Total time to execute: {stop - start:.2f}s")
-###################
-
-### Profiling run ###
-# pr = cProfile.Profile()
-# pr.enable()
-
-# my_result = comparison(**paramdict)
-
-# pr.disable()
-# s = io.StringIO()
-# ps = pstats.Stats(pr, stream=s).sort_stats('cumtime')
-# ps.print_stats()
-
-# with open("AutoML/ScikitLearn/parSim_v5/profile_report.prof", 'w') as f:
-#     f.write(s.getvalue())
-#####################
---
+++ AutoML/ScikitLearn/workspace/e.py
@@ -0,0 +1,14 @@
+### Profiling run ###
+# pr = cProfile.Profile()
+# pr.enable()
+
+# my_result = comparison(**paramdict)
+
+# pr.disable()
+# s = io.StringIO()
+# ps = pstats.Stats(pr, stream=s).sort_stats('cumtime')
+# ps.print_stats()
+
+# with open("AutoML/ScikitLearn/parSim_v5/profile_report.prof", 'w') as f:
+#     f.write(s.getvalue())
+#####################
