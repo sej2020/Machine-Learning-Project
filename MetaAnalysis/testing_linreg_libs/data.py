4ddb48f6dfe585fe5e8258ee1cbdae0c0e8382a5
sj110 <sj110@iu.edu>
2023-02-23 16:57:17 -0500

    data

---
+++ MetaAnalysis/testing_linreg_libs/data.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import sys
+
+def make_data_circle(resolution,radius):
+    xr = np.arange(-radius,radius,resolution)
+    pairs = [[radius,0]]
+    for x in xr:
+        y = math.sqrt(radius**2 - x**2)
+        pairs.append([x,y])
+        pairs.append([x,-y])
+    return pairs
+
+def make_line(data):
+    print(np.size(data))
+    X = data[:,0]
+    Y = data[:,1]
+    k,d = np.polyfit(X,Y,1)
+    # y_hat = k*X + d
+    # plt.gca().set_aspect('equal')
+    # plt.title(f"{d:e} {k:e}")
+    # plt.plot(X,Y,'.')
+    # plt.plot(X,y_hat)
+    # plt.show()
+    return k,d
+
+#test extreme values
+# x,y = sys.float_info.max,sys.float_info.min
+# matrix1 = np.array([[x,y],[-x,-y]])
+# matrix2 = np.array([[x,x],[-x,-x]])
+# print(make_line(matrix1))
+# print(make_line(matrix2))
+
+#test with shapes
+print(make_line(np.array(make_data_circle(resolution = 0.00000001,radius = 1))))
