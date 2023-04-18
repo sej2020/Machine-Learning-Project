4bea41d5a5cdae9f6116e2949b9f4cd873c0f59c
sj110 <sj110@iu.edu>
2023-04-18 18:57:12 -0400

    polynomial visualization

--- BetaDataExper/HyperEllipsoid/closed_form_figs/closed_form.py
+++ BetaDataExper/HyperEllipsoid/closed_form_figs/closed_form.py
@@ -8,28 +8,29 @@ import time
 
 # ["location_x", "location_y", "rotation", "major_axis_length", "minor_axis_length", "MAE"]
 
-def make_viz(array, deg, trained_model):
+def make_viz(array, deg, trained_model, dummys):
 
-    for loc_x, loc_y in [(0,0), (0,10), (10,0), (10,10)]:
+    for (loc_x, loc_y), dummy in zip([(0,0), (0,10), (10,0), (10,10)], dummys):
         x_rot = []
         y_min_ax = []
         z_err = []
-        to_predict = []
+        # to_predict = []
         for row in array:
             if row[1] == loc_x and row[2] == loc_y:
-                to_predict.append(row[:-1])
+            #     to_predict.append(row[:-1])
                 x_rot.append(row[3])
                 y_min_ax.append(row[5])
                 z_err.append(row[-1])
         fig = plt.figure() 
         ax = fig.add_subplot(projection='3d')
         ax.scatter(x_rot, y_min_ax, z_err, label="Actual")
-        ax.scatter(x_rot, y_min_ax, trained_model.predict(to_predict), label="Prediction")
+        ax.scatter(dummy[:,3], dummy[:,5], trained_model.predict(dummy), label="Prediction", marker="_")
         ax.legend()
         ax.set_title(f"Ellipses at location: ({loc_x}, {loc_y})")
         ax.grid()
         ax.set_xlabel("Rotation")
         ax.set_ylabel("Minor Axis size")
+        ax.set_zlim(0,12)
         ax.set_zlabel("MAE")
         plt.show()
         fig.savefig(f"BetaDataExper/HyperEllipsoid/closed_form_figs/polydeg{deg}/({loc_x},{loc_y})")
@@ -48,9 +49,12 @@ def main(datapath):
     # make_viz(array, 1, lin)
 
     for deg in range(1, 6):
+
+        dummy_Xs = interp_data(deg)
+
         poly = PolynomialFeatures(degree = deg)
         X_poly = poly.fit_transform(X)
-        
+
         poly.fit(X_poly, y)
         lin_poly = LinearRegression()
         lin_poly.fit(X_poly, y)
@@ -58,13 +62,36 @@ def main(datapath):
         y_pred = lin_poly.predict(X_poly)
         print(f'MAE of polynomial degree {deg}: {mean_absolute_error(y, y_pred)}')
 
-        print(X_poly.shape, y.shape)
         new_array = np.append(X_poly, np.expand_dims(y, axis=1), axis=1)
-        make_viz(new_array, deg, lin_poly)
+        make_viz(new_array, deg, lin_poly, dummy_Xs)
 
 # 4 3-D plots
 
+# ["location_x", "location_y", "rotation", "major_axis_length", "minor_axis_length", "MAE"]
+
+def interp_data(deg):
+    dummy = PolynomialFeatures(degree = deg)
+    ph00 = np.zeros(shape=(10000,5))
+    ph010 = np.zeros(shape=(10000,5))
+    ph100 = np.zeros(shape=(10000,5))
+    ph1010 = np.zeros(shape=(10000,5))
+
+    ph010[:, 1] = np.array([10 for _ in range(10000)])
+    ph100[:, 0] = np.array([10 for _ in range(10000)])
+    ph1010[:, 0] = np.array([10 for _ in range(10000)])
+    ph1010[:, 1] = np.array([10 for _ in range(10000)])
+
+    dummy_Xs = []
+    for ph in [ph00, ph010, ph100, ph1010]:
+        ph[:, 2] = np.array([i/100 for i in range(0, 90*100, 90) for j in range(100)])
+        ph[:, 3] = np.array([10 for _ in range(10000)])
+        ph[:, 4] = np.array([i/100 for j in range(100) for i in range(1*100, 10*100, 9)])
+        X_dummy = dummy.fit_transform(ph)
+        dummy_Xs.append(X_dummy)
+    
+    return dummy_Xs
+
 
 if __name__ == "__main__":
-    datapath = "BetaDataExper\HyperEllipsoid\pred_error.csv"
+    datapath = "BetaDataExper/HyperEllipsoid/closed_form_figs/pred_error.csv"
     main(datapath)
