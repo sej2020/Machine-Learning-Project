29fb3e292900d12068cd9125070afb4cda123e8d
Joshua-Elms <bacon.joshy@gmail.com>
2023-05-23 22:35:44 -0400

    some stuff for samothy

--- dcai_comparison/get_avg.py
+++ dcai_comparison/get_avg.py
@@ -1,10 +1,11 @@
 import pandas as pd
 from pathlib import Path
 
-dir = Path("dcai_comparison/comp_tests")
+folder = "cholesterol"
+dir = Path("dcai_comparison/comp_tests/{folder}")
 best = []
 
-for file in dir.glob("ld_run*.csv"):
+for file in dir.glob("run*.csv"):
     df = pd.read_csv(file, index_col=0, header=0)
     best_rmse = df.iloc[0, 0]
     best.append(best_rmse)
