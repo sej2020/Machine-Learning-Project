246c440a5f5c383da2f3e6cce08fc5489fe1cf98
sj110 <sj110@iu.edu>
2023-05-27 10:52:08 -0700

    case studies

--- AutoML/ScikitLearn/parSim_v5/main.py
+++ AutoML/ScikitLearn/parSim_v5/main.py
@@ -1,12 +1,12 @@
 from utils import *
 
 paramdict = {'id': 30,
-            'datapath': 'dcai_comparison/csv_data/analcatdata_negotiation.csv', # ConductivityData/train.csv'
+            'datapath': 'dcai_comparison/csv_data/concrete_train.csv', # ConductivityData/train.csv'
             'which_regressors': {'ARDRegression': 1, 'AdaBoostRegressor': 1, 'BaggingRegressor': 1, 'BayesianRidge': 1, 'CCA': 0, 
                                  'DecisionTreeRegressor': 1, 'DummyRegressor': 0, 'ElasticNet': 1, 'ExtraTreeRegressor': 1, 
                                  'ExtraTreesRegressor': 1, 'GammaRegressor': 1, 'GaussianProcessRegressor': 0, 'GradientBoostingRegressor': 1, 
                                  'HistGradientBoostingRegressor': 1, 'HuberRegressor': 1, 'IsotonicRegression': 1, 'KNeighborsRegressor': 1, 
-                                 'KernelRidge': 1, 'Lars': 1, 'Lasso': 1, 'LassoLars': 1, 'LassoLarsIC': 1, 'LinearRegression': 1, 
+                                 'KernelRidge': 0, 'Lars': 1, 'Lasso': 1, 'LassoLars': 1, 'LassoLarsIC': 1, 'LinearRegression': 1, 
                                  'LinearSVR': 1, 'MLPRegressor': 0, 'MultiTaskElasticNet': 0, 'MultiTaskLasso': 0, 'NuSVR': 1, 
                                  'OrthogonalMatchingPursuit': 1, 'PLSCanonical': 0, 'PLSRegression': 0, 'PassiveAggressiveRegressor': 1, 
                                  'PoissonRegressor': 1, 'QuantileRegressor': 0, 'RANSACRegressor': 0, 'RadiusNeighborsRegressor': 1, 
